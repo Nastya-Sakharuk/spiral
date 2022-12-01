1f672570a1e72bd2933813402876319f7bc4de8a
Anastasiya Sakharuk <sakharuk333@gmail.com>
2022-12-01 13:47:42 +0300

    Spiral

--- src/main/java/com/epam/rd/autotasks/Spiral.java
+++ src/main/java/com/epam/rd/autotasks/Spiral.java
@@ -1,7 +1,45 @@
 package com.epam.rd.autotasks;
 
-class Spiral {
-    static int[][] spiral(int rows, int columns) {
-        return null;
+public class Spiral {
+    public static void main(String[] args) {
+        int[][] array = Spiral.spiral(5, 6);
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.printf("%4s", array[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    public static int[][] spiral ( int rows, int columns){
+        int[][] arr = new int[rows][columns];
+        int num = 1;
+        int max = rows * columns;
+
+        int rmin = 0, rmax = rows - 1;
+        int cmin = 0, cmax = columns - 1;
+
+        while (num <= max) {
+            // проход вправо по верхней строке, инкремент столбцов
+            for (int i = cmin; num <= max && i <= cmax; i++) {
+                arr[rmin][i] = num++;
+            }
+            rmin++;
+            // проход вниз по правому краю, инкремент строк
+            for (int i = rmin; num <= max && i <= rmax; i++) {
+                arr[i][cmax] = num++;
+            }
+            cmax--;
+            // проход влево по нижней строке, декремент столбцов
+            for (int i = cmax; num <= max && i >= cmin; i--) {
+                arr[rmax][i] = num++;
+            }
+            rmax--;
+            // проход вверх по левому краю, декремент строк
+            for (int i = rmax; num <= max && i >= rmin; i--) {
+                arr[i][cmin] = num++;
+            }
+            cmin++;
+        }
+        return arr;
     }
 }
